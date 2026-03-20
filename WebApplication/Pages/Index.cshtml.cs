da810dcb9bf8f695d28f13fee4485bc2ac70ddec
Xavier Olivares <106448901+Luigi573@users.noreply.github.com>
2026-03-20 16:37:25 -0600

    Add Student Deactivation

--- WebApplication/Pages/Index.cshtml.cs
+++ WebApplication/Pages/Index.cshtml.cs
@@ -17,6 +17,8 @@ namespace WebApplication.Pages
         private readonly AppDbContext _context;
         private readonly UserManager<Teacher> _userManager;
         public IList<Student> Students { get; set; } = new List<Student>();
+        [BindProperty]
+        public IList<int> SelectedStudents { get; set; } = new List<int>();
 
         public IndexModel(AppDbContext context, UserManager<Teacher> userManager)
         {
@@ -32,6 +34,26 @@ namespace WebApplication.Pages
             {
                 Students = await _context.Students.Where(s => s.SchoolId == teacher.SchoolId && s.IsActive).ToListAsync();
             }
+        }
+
+        public async Task<IActionResult> OnPostDeactivateAsync()
+        {
+            var teacher = await _userManager.GetUserAsync(User);
+
+            if (teacher != null)
+            {
+                var studentsToDeactivate = await _context.Students.Where(s => SelectedStudents.Contains(s.Id)).ToListAsync();
+
+                foreach (var student in studentsToDeactivate)
+                {
+                    student.IsActive = false;
+                }
+
+                await _context.SaveChangesAsync();
+                return RedirectToPage();
+            }
+
+            return Forbid();
         }
     }
 }
