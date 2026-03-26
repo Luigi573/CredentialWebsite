49b494c132481a91669c824532e2bb8bca7030be
Xavier Olivares <106448901+Luigi573@users.noreply.github.com>
2026-03-26 13:13:31 -0600

    Add School Year advancement

--- WebApplication/Pages/Admin/Index.cshtml.cs
+++ WebApplication/Pages/Admin/Index.cshtml.cs
@@ -10,7 +10,8 @@ namespace WebApplication.Pages.Admin
     public class ManageUsersModel : PageModel
     {
         private readonly AppDbContext _context;
-        public IList<Teacher> Teachers { get; set; } = default!;
+        public IList<Teacher> Teachers { get; set; } = new List<Teacher>();
+        public SchoolYear CurrentSchoolYear { get; set; } = new SchoolYear();
 
         public ManageUsersModel(AppDbContext context)
         {
@@ -20,25 +21,65 @@ namespace WebApplication.Pages.Admin
         public async Task OnGetAsync()
         {
             Teachers = await _context.Users.Include(teacher => teacher.School).ToListAsync();
+            CurrentSchoolYear = _context.SchoolYears.Where(sy => sy.IsActive).FirstOrDefault() ?? new SchoolYear();
         }
 
         public async Task<IActionResult> OnPostDeleteAsync(string? id)
         {
-            if (id == null)
+            if (id != null)
             {
-                return NotFound();
+                var teacher = await _context.Users.FindAsync(id);
+
+                if (teacher == null)
+                {
+                    return NotFound();
+                }
+
+                _context.Users.Remove(teacher);
+                await _context.SaveChangesAsync();
+                return RedirectToPage();
             }
 
-            var teacher = await _context.Users.FindAsync(id);
+            return NotFound();
+        }
 
-            if (teacher == null)
+        public async Task<IActionResult> OnPostAdvanceYearAsync()
+        {
+            if(CurrentSchoolYear.StartDate <= DateTime.Now && CurrentSchoolYear.EndDate >= DateTime.Now)
             {
-                return NotFound();
+                //Update students
+                var students = await _context.Students.ToListAsync();
+
+                foreach (var student in students)
+                {
+                    if ((student.Semester + 2) < 6)
+                    {
+                        student.Semester += 2;
+                    }
+                    else
+                    {
+                        student.IsActive = false;
+                    }
+                }
+
+                var oldYear = await _context.SchoolYears.Where(sy => sy.IsActive).FirstOrDefaultAsync();
+                oldYear?.IsActive = false;
+
+                SchoolYear newYear = new SchoolYear
+                {
+                    Name = $"{DateTime.Now.Year}-{DateTime.Now.Year + 1}",
+                    StartDate = new DateTime(DateTime.Now.Year, 8, 15),
+                    EndDate = new DateTime(DateTime.Now.Year + 1, 1, 5),
+                    IsActive = true
+                };
+
+                _context.SchoolYears.Add(newYear);
+                await _context.SaveChangesAsync();
+
+                return RedirectToPage();
             }
 
-            _context.Users.Remove(teacher);
-            await _context.SaveChangesAsync();
-            return RedirectToPage();
+            return Forbid();
         }
     }
 }
